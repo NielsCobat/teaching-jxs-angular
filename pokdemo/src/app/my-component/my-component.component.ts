44e26312569e6b372d5519fb9507c7ade5aaa23a
18001318 <niels.cobat@etudiant.univ-rennes1.fr>
2023-03-03 10:01:41 +0100

    avancée dans le projet RAPPEL : localhost:4200

---
+++ pokdemo/src/app/my-component/my-component.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { Pokemon } from '../pokemon';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent {
+  id: string = '';
+  pokemons: Pokemon[] = [
+      {
+        id: "1",
+        name: "Pikachu"
+      },
+      {
+        id: "2",
+        name: "charizard"
+      },
+      {
+        id: "3",
+        name: "onyx"
+      },
+      {
+        id: "4",
+        name: "Psyduck"
+      }
+    ]
+}
